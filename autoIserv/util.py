2aa577fba297faf138ffc99b33b0262c10d6c451
RedstoneMedia <34373974+RedstoneMedia@users.noreply.github.com>
2021-04-13 22:40:12 +0200

    Now transmitting not only secure cookies for download, since they seem to be required.

--- autoIserv/util.py
+++ autoIserv/util.py
@@ -5,7 +5,7 @@ def download_file_from_iserv(driver, download_url):
     cookies = driver.get_cookies()
     real_cookies = dict()
     for cookie in cookies:
-        if cookie["secure"] == True:
+        if cookie["secure"] == True or (not cookie["secure"] and not cookie["httpOnly"]):
             real_cookies[cookie["name"]] = cookie["value"]
 
     result = requests.get(download_url, cookies=real_cookies, allow_redirects=True)
